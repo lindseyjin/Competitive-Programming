ecdfcade581595a76be47e0e171933be865d544f
Lindsey Jin <l44jin@uwaterloo.ca>
2018-03-01 13:19:45 -0500

    first commit

---
+++ pairsquestion.cc
@@ -0,0 +1,24 @@
+#include <unordered_map>
+#include <iostream>
+using namespace std;
+
+vector <vector <int> > returnpairs (vector <int> nums, int target) {
+     unordered_map<int, boolean> pair;
+    
+    //add to unordered_map
+    for (int x = 0; x < nums.size(); x++) {
+        pair.insert(nums.at(x), false);
+    }
+
+    //iterate through map
+    for (auto it = mymap.begin(); it != mymap.end(); ++it) {
+
+    }
+
+    
+}
+
+int main (int argc, char* argv[]) {
+    
+    return 0;
+}

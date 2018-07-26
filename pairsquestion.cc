1cae4c5167716ca1d455b66252a6f1e67d632705
root <l44jin@edu.uwaterloo.ca>
2018-07-26 18:59:10 -0400

    testing

--- pairsquestion.cc
+++ pairsquestion.cc
@@ -1,24 +1,45 @@
 #include <unordered_map>
 #include <iostream>
+#include <vector>
+#include <iterator>
 using namespace std;
 
-vector <vector <int> > returnpairs (vector <int> nums, int target) {
-     unordered_map<int, boolean> pair;
+//Question: In a given set of numbers, find all unique map that add up to target
+vector <vector <int> > returnmap (vector <int> nums, int target) {
+     unordered_map<int, boolean> map;
     
     //add to unordered_map
     for (int x = 0; x < nums.size(); x++) {
-        pair.insert(nums.at(x), false);
+        map.insert(nums.at(x), false);
     }
 
-    //iterate through map
-    for (auto it = mymap.begin(); it != mymap.end(); ++it) {
-
+    auto it = map.find(key);
+    //iterate through array
+    for (int x = 0; x < nums.size(); x++) {
+        if ((target - nums.at(x)) != map.end()) 
+        it->second = true;
     }
-
     
+    vector <vector <int> > pairs;
+    for (auto it2 = map.begin(); it2 != map.end(); it2++) {
+        if (it2->second == true) {
+            vector <int> v;
+            v.push_back(it2->first);
+            v.push_back(target-it2->first);
+            pairs.push_back(v);
+            cout << it2->first << ", " target - (it2->first) << endl; 
+        }
+    }
+
+    return pairs;
+    //Complexity of solution is 
 }
 
 int main (int argc, char* argv[]) {
+    vector <int> v;
+    for (int x = 1; x < 22; x++) 
+        v.push_back(x);
     
+    vector <vector <int> > pairs = returnmap(v, 18);
     return 0;
 }
